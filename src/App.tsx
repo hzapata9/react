9b9729662a5bc992064c4c57b7a71def78d927e3
Hector Zapata <hezapata@falabella.cl>
2024-09-12 13:07:46 -0300

    Form bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,71 @@
 import React from 'react';
 import './index.css';
-import Employee  from "./components/Employee";
-import { useState} from "react";
+import Employee from "./components/Employee";
+import {useState} from "react";
+import {v4 as uuidv4} from "uuid";
 
 function App() {
     console.log('we are about to list the employees');
     const [role, setRole] = useState("Doctor");
-
+    const [employees, setEmployees] = useState(
+        [
+            {
+                name: "Hector",
+                role: "developer",
+                img: "https://images.pexels.com/photos/5668869/pexels-photo-5668869.jpeg"
+            },
+            {
+                name: "Marie",
+                role: "developer",
+                img: "https://images.pexels.com/photos/7491620/pexels-photo-7491620.jpeg"
+            },
+            {
+                name: "John",
+                role: "developer",
+                img: "https://images.pexels.com/photos/5668869/pexels-photo-5668869.jpeg"
+            },
+            {
+                name: "Abby",
+                role: "developer",
+                img: "https://images.pexels.com/photos/7491620/pexels-photo-7491620.jpeg"
+            },
+            {
+                name: "Peter",
+                role: "developer",
+                img: "https://images.pexels.com/photos/5668869/pexels-photo-5668869.jpeg"
+            },
+            {
+                name: "Alexa",
+                role: "developer",
+                img: "https://images.pexels.com/photos/7491620/pexels-photo-7491620.jpeg"
+            },
+        ]
+    )
     const showEmployees = true;
-  return (
-    <div className="App bg-red-300">
-        {showEmployees ? (
-        <>
-            <input className="border-8" type="text" onChange={(e) => {
-                console.log(e.target.value);
-                setRole(e.target.value);
-            }} />
-          <Employee name="Hector" role="developer" />
-          <Employee name="Carlos" role={role}/>
-          <Employee name="John"/>
-          <Employee />
-          <Employee />
+    return (
+        <div className="App">
+            {showEmployees ? (
+                <>
 
-        </>
-        ) : (
-            <p>You cannot see the employees</p>
-        )}
-    </div>
-  );
+                    <div className="flex flex-wrap justify-centers">
+                        {employees.map((employee) => {
+                            return (
+                                <Employee
+                                    key={uuidv4()}
+                                    name={employee.name}
+                                    role={employee.role}
+                                    img={employee.img}
+                                />
+                            );
+                        })}
+                    </div>
+
+                </>
+            ) : (
+                <p>You cannot see the employees</p>
+            )}
+        </div>
+    );
 }
 
 export default App;
